291278e01563674d1c9fc70d077f183f079b4ffc
linjinzhen <linjinzhen@hotmail.com>
2017-11-12 16:35:12 +0800

    饿了么已更新获取菜单的请求，代码进行相应调整

--- eleme/spiders/menu.py
+++ eleme/spiders/menu.py
@@ -20,13 +20,16 @@ class MenuSpider(scrapy.Spider):
 
     def start_requests(self):
         cur.execute("""
-            SELECT restaurant_id FROM restaurant_info
+            SELECT restaurant_id, longitude, latitude FROM restaurant_info
             LEFT JOIN (SELECT restaurant_id AS id FROM menu_info) AS t
             ON restaurant_info.restaurant_id = t.id
             WHERE t.id IS NULL
         """)
-        for restaurant_id in cur.fetchall():
-            yield scrapy.Request(self.base_url + str(restaurant_id[0]))
+        for restaurant_info in cur.fetchall():
+            x_shard = "x-shard:shopid=%s;loc=%s,%s" % restaurant_info
+            url = self.base_url + str(restaurant_info[0])
+            headers = {"x-shard": x_shard}
+            yield scrapy.Request(url, headers=headers)
 
     def parse(self, response):
         if 200 == response.status:
