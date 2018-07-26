668d647e1c02fed96d5d8deb20c9c49f2b347cce
linjinzhen <linjinzhen@hotmail.com>
2018-07-26 23:57:52 +0800

    说明美团参数构造加密方法，以及一些无关紧要的小调整

--- waimai/meituan_encryptor.py
+++ waimai/meituan_encryptor.py
@@ -26,7 +26,19 @@ class MeituanEncryptor():
         self.brVD = random.choice(brVD_list)
         self.brR = [self.brVD, self.brVD, 24, 24]
 
+    def compress_data(self, data):
+        """压缩token和sign参数的方法
+        转为JSON字符串，使用zlib压缩，再使用base64编码
+        """
+        json_data = json.dumps(data, separators=(',', ':')).encode("utf-8")
+        compressed_data = zlib.compress(json_data)
+        base64_str = base64.b64encode(compressed_data).decode()
+        return base64_str
+
     def get_sign(self):
+        """构造sign（请求的URL中一个参数）
+        由各种乱七八糟的信息组成的字典，进行URL编码，URL参数拼接，然后压缩
+        """
         clean_data = {
             key: value
             for key, value in self.data.items()
@@ -40,13 +52,10 @@ class MeituanEncryptor():
         self.sign = compressed_data
         return compressed_data
 
-    def compress_data(self, data):
-        json_data = json.dumps(data, separators=(',', ':')).encode("utf-8")
-        compressed_data = zlib.compress(json_data)
-        base64_str = base64.b64encode(compressed_data).decode()
-        return base64_str
-
     def get_token(self, rid=100009):
+        """构造_token（请求的URL中一个参数）
+        由各种乱七八糟的信息（包括sign)组成的字典，转为JSON字符串后压缩
+        """
         token_data = {
             "ver": "1.0.6",
             "aM": "",
@@ -62,20 +71,18 @@ class MeituanEncryptor():
             "tT": [],
             "sign": self.get_sign()
         }
-        """
-        key_order = [
-            "rId", "ver", "ts", "cts", "brVD", "brR", "bI",
-            "mT", "kT", "aT", "tT", "aM", "sign"
-        ]
-        token_data = OrderedDict(
-            sorted(token_data.items(), key=lambda t: key_order.index(t[0]))
-        )
-        """
         compressed_data = self.compress_data(token_data)
         self.token = compressed_data
         return compressed_data
 
     def get_xforwith(self, html_text=None):
+        """构造X-FOR-WITH（headers中的一项）
+        将时间、屏幕像素等多种信息组成的字典（见以下data变量）转为JSON字符串
+        然后使用AES加密，AES加密模式为CBC
+        加密的密钥和IV一样，构造方式为：
+        一开始进入的网页里有个meta元素（一般为页面最后一个meta元素了），id为六位随机字母组成
+        取其content（很长），然后从第0个字符开始，每10个取一个字符，最多取16个字符）
+        """
         if html_text is not None:
             sel = Selector(text=html_text)
             meta_content = sel.xpath("//meta")[-1].xpath("./@content").extract_first()
@@ -99,6 +106,12 @@ class MeituanEncryptor():
         return x_for_with
 
     def get_mta(self, cookies, ua):
+        """构造__mta（cookies中的一项）
+        第一部分：把各种信息的字符串拼起来，然后进行一些神奇的hash处理
+        第二部分-第四部分：由三个时间戳组成，具体哪个代表什么不大清楚，但可以都一样
+        第五部分：从1开始，会随着时间增长一直加，不过就按1来吧
+        以上五部分使用"."来拼接
+        """
         hash_data = "Netscape" + "undefined" + "zh-cn" + "iPhone"
         hash_data += ua
         hash_data += ("0" + "x".join([str(x) for x in self.brVD]))
@@ -129,6 +142,15 @@ class MeituanEncryptor():
         return mta
 
     def get_lxsdk(self, ua):
+        """构造_lxsdk_cuid和_lxsdk（两者一样，都是cookie中的一项）
+        由五部分组成，各部分之间用"-"连接
+        第一部分：时间戳转16进制，字符串再接上200转16进制的字符串
+        第二部分：0-1之间的随机数，转16进制小数，去掉小数点
+        第三部分：User-Agent字符串每四位分段，然后进行一些神奇的处理
+        第四部分：屏幕总像素数量转16进制
+        第五部分：时间戳转16进制，字符串再接上200转16进制的字符串
+        """
+
         def get_part2():
             rnd_num = random.random()
             num = rnd_num
@@ -160,12 +182,18 @@ class MeituanEncryptor():
         return "-".join([part1, part2, part3, part4, part5])
 
     def get_lxsdk_s(self):
+        """构造_lxsdk_s（cookies中的一项）
+        第一部分是时间戳转16进制字符串
+        第二部分-第四部分都是由三个字符组成，每个字符是1-65537之间的随机数
+        转16进制后取第一位（相当于随机16进制字符吧）
+        这四部分用"-"连接，之后又加上"//NaN"（反正每次都是以这个结尾，要URL编码）
+        """
         part_list = []
         part_list.append(hex(round(time.time() * 1000))[2:])
         for i in range(3):
             rnd_hexchr_list = []
             for j in range(3):
-                rnd_hexchr = hex(round(1 + 65536 * random.random()))[3]
+                rnd_hexchr = hex(round(1 + 65536 * random.random()))[2]
                 rnd_hexchr_list.append(rnd_hexchr)
             part_list.append("".join(rnd_hexchr_list))
         return "-".join(part_list) + "%7C%7CNaN"
